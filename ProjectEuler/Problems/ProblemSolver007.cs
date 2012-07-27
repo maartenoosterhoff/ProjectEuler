0f2c33da2d23e5cb7f2a5ca1787afc41c1d883c5
Maarten Oosterhoff <maarten.oosterhoff+github@gmail.com>
2012-07-27 15:49:51 +0200

    Problem 7 added.

---
+++ ProjectEuler/Problems/ProblemSolver007.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Problems {
+    class ProblemSolver007 : ProblemSolverBase {
+        protected override string GetSolution() {
+            return Helper.GetPrimesWithErastosthenesUpTill(1000000) // Take primes up till 1000000, this is a guess
+                .Skip(1) // Skip prime 1 which should not be included according to the question
+                .Skip(10000)
+                .First()
+                .ToString();
+        }
+
+        protected override string GetProblemDescription() {
+            return @"By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?";
+        }
+
+        public override int ProblemNumber {
+            get { return 7; }
+        }
+    }
+}
