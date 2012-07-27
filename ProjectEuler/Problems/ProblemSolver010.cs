e14dcc1cf9938b99795f1009c6df605e5ed9aeef
Maarten Oosterhoff <maarten.oosterhoff+github@gmail.com>
2012-07-27 15:52:00 +0200

    Problem 10 added.

---
+++ ProjectEuler/Problems/ProblemSolver010.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Problems {
+    class ProblemSolver010 : ProblemSolverBase {
+        protected override string GetSolution() {
+            return Helper.GetPrimesWithErastosthenesUpTill(2000000) // Take primes up till 1000000, this is a guess
+                .Skip(1) // Skip prime 1 which should not be included according to the question
+                .Sum()
+                .ToString();
+        }
+
+        protected override string GetProblemDescription() {
+            return @"The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.";
+        }
+
+        public override int ProblemNumber {
+            get { return 10; }
+        }
+    }
+}
