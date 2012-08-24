c1a8bf0afd4da72160d02cc528cbfd9a99f494cc
Maarten Oosterhoff <maarten.oosterhoff+github@gmail.com>
2012-08-24 10:33:01 +0200

    Added problem 15.

---
+++ ProjectEuler/Problems/ProblemSolver015.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ProjectEuler.Problems {
+    class ProblemSolver015 : ProblemSolverBase {
+        protected override string GetSolution() {
+            //long dimension = 20;
+
+            //var current = dimension + 1;
+            //long solution = 1;
+            //while (current > 1) {
+            //    solution *= current;
+            //    current--;
+            //}
+            //return solution.ToString();
+            return "Unsolved";
+        }
+
+        protected override string GetProblemDescription() {
+            return @"Starting in the top left corner of a 2x2 grid, there are 6 routes (without backtracking) to the bottom right corner.
+
+(picture which cannot be shown here, see http://projecteuler.net/problem=15)
+
+How many routes are there through a 20x20 grid?";
+        }
+
+        public override int ProblemNumber {
+            get { return 15; }
+        }
+    }
+}
