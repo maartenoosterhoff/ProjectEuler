4f8330eebf063a8a18377c2e22f30bca4c853a11
Maarten Oosterhoff <maartenoosterhoff@funda.nl>
2017-04-25 15:07:05 +0200

    Added progress to problem 15.

--- ProjectEuler/Problems/ProblemSolver015.cs
+++ ProjectEuler/Problems/ProblemSolver015.cs
@@ -4,6 +4,38 @@
     {
         protected override string GetSolution()
         {
+
+            const int SIZE = 3;
+            int[,] grid = new int[SIZE + 1, SIZE + 1];
+
+            for (int x = 0; x < SIZE + 1; x++)
+            {
+                for (int y = 0; y < SIZE + 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        grid[0, 0] = 1;
+                    }
+                    else if (x == 0)
+                    {
+                        grid[0, y] = grid[0, y - 1];
+                    }
+                    else if (y == 0)
+                    {
+                        grid[x, 0] = grid[x - 1, 0];
+                    }
+                    else
+                    {
+                        grid[x, y] = grid[x - 1, y] + grid[x, y - 1];
+                    }
+                }
+            }
+
+            var result = grid[SIZE, SIZE].ToString();
+
+            return result;
+
+
             //long dimension = 20;
 
             //var current = dimension + 1;
